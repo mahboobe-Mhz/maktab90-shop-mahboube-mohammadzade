db1a0c7a35730ca187eab3a6df118c0b096da23e
mahbo0be mohammadzade <mahbo0be.mohammadzade@gmail.com>
2023-06-06 18:31:11 +0330

    add sidebar of product managment

--- src/screens/main/admin/products-management/index.tsx
+++ src/screens/main/admin/products-management/index.tsx
@@ -1,19 +1,32 @@
-import { Box } from "@mui/material";
+import { Box,Typography ,Button} from "@mui/material";
+import SideBar from "../../../../components/adminComponent/sideBar";
+import KeyboardArrowLeftIcon from '@mui/icons-material/KeyboardArrowLeft';
 
 const ProductsManagement = () => {
-    return ( <Box sx={{display:"flex"}}>
+    return ( <Box sx={{display:"flex", direction:"rtl"}}>
+       
         <Box>
-        <Box>
+            <SideBar/>
+        </Box>
+  
 
+        <Box sx={{padding:"15px", bgcolor:"#F3F3F5", width:"100%"}}>
+            <Box sx={{display:"flex"}}>
+                <Typography sx={{color:"secondary.main"}}> برنامه ها</Typography>
+                <KeyboardArrowLeftIcon/>
+                <Typography>  فروشگاه</Typography>
+               
+            </Box>
+            <Box sx={{display:"flex", justifyContent:"space-between", marginTop:"20px", bgcolor:"#ffff", alignItems:"center", paddingX:"20px", paddingY:"5px", borderRadius:"20px"}}>
+                <Typography> محصولات</Typography>
+                <Button sx={{color:"#ffff", paddingX:"30px", bgcolor:"secondary.main", borderRadius:"50px"}}> افزودن محصول</Button>
+            </Box>
+            <Box>
+                
+            </Box>
         </Box>
-        <Box>
-
-        </Box>
-        
-        </Box>
-        <Box>
             
-        </Box>
+        
     </Box> );
 }
  

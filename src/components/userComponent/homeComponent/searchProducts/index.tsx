fc0ec6f8d7d64611fe0dcda7e55a04c52a2690b6
mahbo0be mohammadzade <mahbo0be.mohammadzade@gmail.com>
2023-10-25 16:59:06 +0330

    handle search product section to have defualt chosen

--- src/components/userComponent/homeComponent/searchProducts/index.tsx
+++ src/components/userComponent/homeComponent/searchProducts/index.tsx
@@ -2,16 +2,21 @@ import React from "react";
 import ProductsSlider from "./sliderProducts";
 
 const SearchProducts = () => {
+    const [state, setState]=React.useState("تخفیفات ویژه")
+    const productsSort= ["محصولات پرفروش", "تخفیفات ویژه","جدیدترین محصولات"]
     return ( <div className="flex flex-col items-center md:mt-10">
         <h1 className="text-secondary md:text-4xl text-3xl font-bold md:mb-5">جستجوی محصول</h1>
         <div  className=" relative md:mt-10  mt-6 flex gap-2 md:gap-5">
-            <button className="relative  focus:bg-secondary focus:text-white focus:outline-none
-             focus:ring-none border-2 px-2
-             rounded-full pb-1 font-semibold border-secondary text-xs md:text-lg" >محصولات پرفروش</button>
-            <button className="relative focus:bg-secondary focus:text-white  focus:outline-none focus:ring-none border-2 px-2
-             rounded-full pb-1 font-semibold border-secondary text-xs md:text-lg" > تخفیفات ویژه</button>
-            <button className="relative  focus:bg-secondary focus:text-white focus:outline-none focus:ring-none border-2 px-2
-             rounded-full pb-1 font-semibold border-secondary text-xs md:text-lg" >جدیدترین محصولات  </button>
+            {
+                productsSort.map((item:string)=>
+                state===item?<span onClick={()=>setState(item)}  className="relative 
+               border-2 border-secondary px-2 cursor-pointer
+                rounded-full pb-1 font-semibold bg-secondary text-white text-xs md:text-lg" >{item} </span>:
+                <span onClick={()=>setState(item)}  className="relative  
+              border-2 px-2 cursor-pointer
+                rounded-full pb-1 font-semibold border-secondary text-xs md:text-lg" > {item}</span>)
+            }
+    
             
         </div>
         <div className="w-[100%] flex items-center justify-center  mt-6 ">

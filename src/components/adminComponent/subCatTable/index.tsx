f6f72141e6794f09170cc980db8a029f75d85b0b
mahbo0be mohammadzade <mahbo0be.mohammadzade@gmail.com>
2023-11-11 09:24:39 +0330

    edit add subCategory page

--- src/components/adminComponent/subCatTable/index.tsx
+++ src/components/adminComponent/subCatTable/index.tsx
@@ -1,101 +1,128 @@
-import { Box, Checkbox, FormControl, FormControlLabel, FormLabel, Paper, Radio, RadioGroup, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography } from '@mui/material'
+import { Box, Checkbox, FormControl, FormControlLabel, FormLabel, Paper, Radio, RadioGroup, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, TextField, Typography } from '@mui/material'
 import DeleteOutlineOutlinedIcon from "@mui/icons-material/DeleteOutlineOutlined";
 import ModeEditOutlineOutlinedIcon from "@mui/icons-material/ModeEditOutlineOutlined";
 import useGetAllCategory from '../../../api/services/products/useGetAllCategory';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import Select, { SelectChangeEvent } from '@mui/material/Select';
+import { Category } from '../../../api/interface/category';
+import axios from 'axios';
 
 function SubCatTable() {
     const { data, isLoading } = useGetAllCategory();
     const [subCatName, setSubCatName]=useState("")
-    const [adminInfo , setAdminInfo]=useState<string[]>([])
-    const handleInsertSubCat=()=>{
-      setAdminInfo([...adminInfo,subCatName])
-    }
-    const handleChangeCat =()=>{
-      setAdminInfo([])  
-    }
+    const [category, setCategory] = useState('');
+
+    const handleChange = (event: SelectChangeEvent) => {
+      setCategory(event.target.value);
+    };
+
+      
+
   return (
     <div>
-         <Box  bgcolor={"#ffff"} borderRadius={"20px"} paddingBottom={2} display={"flex"} gap={5} >
-              <Box sx={{display:"flex", flexDirection:"column"}} >    
-   <FormControl>
-   <FormLabel id="demo-radio-buttons-group-label" sx={{marginRight:"30px", fontSize:"25px"}}>دسته ها</FormLabel>
-   <RadioGroup
-     aria-labelledby="demo-radio-buttons-group-label"
-     defaultValue="female"
-     name="radio-buttons-group"
-   >
-{
-     !isLoading && data.data.categories?.map((item:any) => ( <FormControlLabel    key={item._id}   value={item._id} 
-      control={<Radio  
-        onClick={handleChangeCat}
-        sx={{
-        '&, &.Mui-checked': {
-          color: 'secondary.main',
-        },
-      }} />}       label={item.name} />)) 
-}
-
-   </RadioGroup>
- </FormControl>          
-              </Box>
-              <Box>
-              <div className='flex md:gap-10 gap-5 md:flex-row flex-col'>
-        <div className='flex  gap-5 text-right'>
-    <label className='pr-2 text-2xl' htmlFor="">   زیر دسته</label>
-    <input onChange={(e)=>setSubCatName(e.currentTarget.value)} type="text" className='border border-secondary rounded-full px-3 py-1 mt-4' />
-    <button onClick={handleInsertSubCat} className='bg-secondary text-white rounded-full md:px-10 h-[40px] md:mt-4'> +</button>
-  </div>
-
+         <Box
+            sx={{
+              display: "flex",
+              justifyContent: "space-between",
+              marginTop: "20px",
+              bgcolor: "#ffff",
+              alignItems: "center",
+              paddingX: "20px",
+              paddingY: "5px",
+              borderRadius: "20px",
+              marginBottom:"20px"
+            }}
+          >
+            <Typography
+              sx={{
+                fontSize: {
+                  lg: 30,
+                  md: 30,
+                  sm: 20,
+                  xs: 20,
+                },
+                color: "secondary.main",
+        
+              }}
+            >
+              {" "}
+            زیر دسته ها
+            </Typography>
+          </Box>
+         <Box  bgcolor={"#ffff"} borderRadius={"20px"} paddingBottom={2} display={"flex"} gap={20} >
+              <Box sx={{display:"flex"}} >    
+              <FormControl variant="standard" sx={{ m: 1, minWidth: 120 }}>
+        <InputLabel id="demo-simple-select-standard-label">دسته ها</InputLabel>
+        <Select
+          labelId="demo-simple-select-standard-label"
+          id="demo-simple-select-standard"
+          value={category}
+          onChange={handleChange}
+          label="Age"
+        >
+          {
+          !isLoading && data.data.categories?.map((item:Category)=>     <MenuItem value={item.name}>
+         {item.name}
+          </MenuItem>)
+          }
        
-        </div>
-
-           
-             <TableContainer  component={Paper} sx={{marginTop:"10px"}}>
- 
-             <Table sx={{ minWidth: 650 ,}} aria-label="simple table">
-               <TableHead>
-                 <TableRow> 
-                 <TableCell align="right">  زیر دسته ها</TableCell>
-           
-                  
-                     <TableCell align="right"> </TableCell>
-               
-                 </TableRow>
-               </TableHead>
-               <TableBody>
-            {
-               adminInfo.length>0 && adminInfo.map((item:any,index:number)=>  
-               <TableRow
-               
-               sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-              >
-               <TableCell align="right">{item}</TableCell>
-
-              
-               <TableCell align="left">   
-                  <DeleteOutlineOutlinedIcon
-                  id={item.adminName}
-                
-                       sx={{ color: "secondary.main" }}
-                     />
-                     <ModeEditOutlineOutlinedIcon
-              
-                       sx={{ color: "secondary.main" }}
-                     /> 
-                     </TableCell>
-              
-                              </TableRow>)
-              
-            }
-                  
-              
-               </TableBody>
-             </Table>
-            </TableContainer>
-            <button className='text-white bg-secondary px-20 py-1.5 rounded-full w-[200px] mx-auto mt-5 mb-10'> ثبت</button>
+     
+        </Select>
+      </FormControl>
+      <Box> 
+      <TextField id="standard-basic"  onChange={(e)=>setSubCatName(e.currentTarget.value)} label="زیردسته" variant="standard" sx={{marginTop:"8px"}}/>
+      </Box>
+      <button className='text-white bg-secondary  py-1.5 rounded-full w-[90px] mx-auto mt-5 mb-10 mr-10'> ثبت</button>
               </Box>
+     
     </Box> 
+
+    <Box bgcolor={"#ffff"} borderRadius={"20px"} paddingBottom={2} marginTop={2}>
+    <TableContainer  component={Paper}>
+
+      <Table sx={{ minWidth: 650 ,}} aria-label="simple table">
+        <TableHead>
+          <TableRow> 
+          <TableCell align="right">دسته</TableCell>
+              <TableCell align="right">زیر دسته </TableCell>
+              <TableCell align="center">  </TableCell>
+          
+         
+        
+          </TableRow>
+        </TableHead>
+        <TableBody>
+     {
+      !isLoading && data.data.categories?.map((item:Category)=>    <TableRow
+        
+      sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+    >
+      <TableCell align="right">{item.name}</TableCell>
+      <TableCell align="center">
+        <TableRow sx={{display:"flex", flexDirection:"column"}}>
+          
+          <TableCell sx={{ display:"flex" , justifyContent:"space-between" , }}> {item.name} <Box>  <DeleteOutlineOutlinedIcon sx={{ color: "secondary.main" }} /> <ModeEditOutlineOutlinedIcon id={item._id}  sx={{ color: "secondary.main" }} />  </Box> </TableCell>
+          <TableCell sx={{ display:"flex" , justifyContent:"space-between" , }}> {item.name} <Box>  <DeleteOutlineOutlinedIcon sx={{ color: "secondary.main" }} /> <ModeEditOutlineOutlinedIcon id={item._id}  sx={{ color: "secondary.main" }} />  </Box> </TableCell>
+          <TableCell sx={{ display:"flex" , justifyContent:"space-between" , }}> {item.name} <Box>  <DeleteOutlineOutlinedIcon sx={{ color: "secondary.main" }} /> <ModeEditOutlineOutlinedIcon id={item._id}  sx={{ color: "secondary.main" }} />  </Box> </TableCell>
+
+ 
+ 
+        </TableRow><Box></Box>
+        
+         </TableCell>
+
+
+  
+    </TableRow>
+)
+     }
+        
+        </TableBody>
+      </Table>
+    </TableContainer>
+    </Box>
     </div>
   )
 }

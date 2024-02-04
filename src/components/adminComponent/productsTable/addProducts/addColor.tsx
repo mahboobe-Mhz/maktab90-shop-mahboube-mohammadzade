171a8f0ceaa76a1994d56f50a1b1c09b4f320322
mahbo0be mohammadzade <mahbo0be.mohammadzade@gmail.com>
2024-02-04 10:55:53 +0330

    insert filter box to home store

--- src/components/adminComponent/productsTable/addProducts/addColor.tsx
+++ src/components/adminComponent/productsTable/addProducts/addColor.tsx
@@ -19,18 +19,7 @@ const MenuProps = {
   },
 };
 
-const names = [
-  'Oliver Hansen',
-  'Van Henry',
-  'April Tucker',
-  'Ralph Hubbard',
-  'Omar Alexander',
-  'Carlos Abbott',
-  'Miriam Wagner',
-  'Bradley Wilkerson',
-  'Virginia Andrews',
-  'Kelly Snyder',
-];
+
 
 
 
